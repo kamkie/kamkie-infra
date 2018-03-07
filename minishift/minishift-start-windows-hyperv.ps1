5410331eab5427f61536ed317b709f40c96465d8
Kamil Kiewisz <kamkie@outlook.com>
2018-03-07 22:52:39 +0100

    use docker nat in minishift-start-windows-hyperv.ps1

--- minishift/minishift-start-windows-hyperv.ps1
+++ minishift/minishift-start-windows-hyperv.ps1
@@ -1,10 +1,14 @@
 
-$SwitchName=(Get-VMNetworkAdapter -ALL | Where {$_.Status -Eq "Ok"} | Where {$_.SwitchName -NotLike "*nat*" }).SwitchName
-[Environment]::SetEnvironmentVariable("HYPERV_VIRTUAL_SWITCH", $SwitchName, [EnvironmentVariableTarget]::User)
-[Environment]::SetEnvironmentVariable("HYPERV_VIRTUAL_SWITCH", $SwitchName, [EnvironmentVariableTarget]::Process)
+#$SwitchName=(Get-VMNetworkAdapter -ALL | Where {$_.Status -Eq "Ok"} | Where {$_.SwitchName -NotLike "*nat*" }).SwitchName
+#[Environment]::SetEnvironmentVariable("HYPERV_VIRTUAL_SWITCH", $SwitchName, [EnvironmentVariableTarget]::User)
+#[Environment]::SetEnvironmentVariable("HYPERV_VIRTUAL_SWITCH", $SwitchName, [EnvironmentVariableTarget]::Process)
+
+New-NetNat -Name SharedNAT -InternalIPInterfaceAddressPrefix 10.0.75.1/24
+$env:MINISHIFT_ENABLE_EXPERIMENTAL="y"
+$env:HYPERV_VIRTUAL_SWITCH="DockerNAT"
 
 minishift delete -f
-cd /c
+cd c:\
 minishift start   --metrics   --logging   --logtostderr  --cpus 4   --memory 6GB   --disk-size 30GB   --vm-driver hyperv
 
 minishift addons enable admin-user
